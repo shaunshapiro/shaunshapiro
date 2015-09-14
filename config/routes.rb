f499b043fa2ff7595de49cf89dc7521e76e4d3b2
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-09-14 22:11:19 +0000

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Shaunshapiro::Application.routes.draw do
+
+  root 'home_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
