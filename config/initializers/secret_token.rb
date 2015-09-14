8f1444604fdc6dfb490b0c76560c211047633a1f
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-09-14 21:55:45 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shaunshapiro::Application.config.secret_key_base = '04236d5a0d29f1b7c4bf27d4c88e0d46f96cbf6e5234e2dd2dafbea4e6b753770e3d64f9b9478afcc505e0c7f8b86b701d23962b499be000d008ace62b1be970'
